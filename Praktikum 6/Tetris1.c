67a62c75cc6365caaeace77bd04c527b6017cf05
nathanndk <90740297+nathanndk@users.noreply.github.com>
2022-04-03 19:53:37 +0700

    Add files via upload

---
+++ Praktikum 6/Tetris1.c
@@ -0,0 +1,28 @@
+/* Nama Program : Tetris.c
+Deskripsi    : menampilkan susunan karakter asterisk
+Pembuat      : Christian Joshua Nathanael Nadeak - 24060121140120
+Tanggal      : 3 April 2022*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   // Kamus
+   int N,i,j;
+
+   // Algoritma
+   printf("Masukkan jumlah baris bintang yang diinginkan : ");
+   scanf("%d",&N);
+   for(i=1;i<=N;i++){                //{Loop Baris}
+       for(j=N;j>=i;j--){            //{Loop Kolom}
+            printf("*");
+            if(j==i){
+                printf("  --%d",j);
+            }
+       }
+       printf("\n");
+    }
+   return 0;
+}
+
